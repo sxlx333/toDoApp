de246aa485b70ddd8b424151cbb0a543cb5b338b
sxlx333 <knifeq8@gmail.com>
2024-09-04 12:00:21 +0300

    ... element popup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('labarytas');
+const allTaskDOMs = document.querySelectorAll('.task');
+
+for (const taskDOM of allTaskDOMs) {
+    const moreDOM = taskDOM.querySelector('.more');
+    const moreActionsDOM = taskDOM.querySelector('.more-actions');
+
+    if (moreDOM) {
+        moreDOM.addEventListener('click', () => {
+            moreActionsDOM.dataset.visible = true;
+        });
+    }
+
+}
